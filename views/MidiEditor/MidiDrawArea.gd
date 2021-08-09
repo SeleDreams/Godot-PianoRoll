6f12fc3efb6b3540653af78e4388a914d93b5f98
SeleDreams <seledreams.contact@gmail.com>
2021-08-09 18:15:20 +0200

    started adding octave lines

--- views/MidiEditor/MidiDrawArea.gd
+++ views/MidiEditor/MidiDrawArea.gd
@@ -6,10 +6,16 @@ export var timesig_numerator : int = 4
 export var timesig_denominator : int = 4
 export var bpm : int = 120
 export var horizontal_scale : float = 50
+export var vertical_scale : float = 20
+
 export var bars : int
 export var zoom_steps : float = 0.2
-export var max_zoom : float = 500
-export var min_zoom : float = 150
+export var max_h_zoom : float = 500
+export var min_h_zoom : float = 150
+export var max_v_zoom : float = 200
+export var min_v_zoom : float = 100
+export var octave_notes : int = 12
+export var number_octaves : int = 8
 
 onready var timesig : float = timesig_numerator as float / timesig_denominator
 onready var hzoom : float = 1
@@ -19,41 +25,55 @@ var vertical_offset : int
 var horizontal_offset : int
 var draw_width : int
 var draw_height : int
+
 var space_between_bars : float
-var actual_space_of_bar : float
-var total_size : float
+var space_between_notes : float
+
+var total_horizontal_size : float
+var total_vertical_size : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rect_min_size.y = draw_height
 	update_zoom()
 
 func clear_background():
-	draw_rect(Rect2(Vector2(-horizontal_offset,0) + Vector2(horizontal_offset,vertical_offset),Vector2(draw_width,draw_height)),Color(40 as float / 255,40 as float / 255,40 as float / 255,1))
+	draw_rect(Rect2(Vector2.ZERO,Vector2(draw_width,draw_height)),Color(40 as float / 255,40 as float / 255,40 as float / 255,1))
 
 func draw_beats():
 	var bpm_factor = 100 as float / bpm
-	actual_space_of_bar = space_between_bars + (timesig_denominator * bpm_factor)
 	for i in range(1,bars * timesig_numerator):
-			var beat_pos = Vector2(space_between_bars * i / timesig_numerator / timesig_denominator  * bpm_factor  ,0)
-			if beat_pos.x < rect_size.x + horizontal_offset and beat_pos.x > horizontal_offset:
-				draw_line(Vector2(-horizontal_offset,0) + beat_pos,Vector2(-horizontal_offset,0) + beat_pos + Vector2(0,draw_height),Color.gray)
+			var beat_pos = Vector2((space_between_bars * i / timesig_numerator / timesig_denominator * bpm_factor) - horizontal_offset,0)
+			if beat_pos.x < rect_size.x and beat_pos.x > 0:
+				draw_line(beat_pos,beat_pos + Vector2(0,draw_height),Color.gray)
 
 func draw_bars():
 	var bpm_factor = 100 as float / bpm
 	for i in range(1,bars + 1):
-		var bar_pos = Vector2(space_between_bars * i / timesig_denominator * bpm_factor ,0)
-		if bar_pos.x < rect_size.x + horizontal_offset and bar_pos.x > horizontal_offset:
-			draw_line(Vector2(-horizontal_offset,0) + bar_pos,Vector2(-horizontal_offset,0) +  bar_pos + Vector2(0,draw_height),Color.white,2.0)
+		var bar_pos = Vector2((space_between_bars * i / timesig_denominator * bpm_factor) - horizontal_offset ,0)
+		if bar_pos.x < rect_size.x and bar_pos.x > 0:
+			draw_line( bar_pos,bar_pos + Vector2(0,draw_height),Color.white,2.0)
+
+func draw_octaves():
+	for i in range(1,number_octaves * octave_notes + 1):
+		var octave_pos = Vector2(0,space_between_notes * i) - Vector2(0,vertical_offset)
+		var line_size : int = 1 if i % octave_notes != 0 else 2
+		draw_line(octave_pos,octave_pos + Vector2(draw_width,0),Color.white,line_size)
+#		for j in range(0,octave_notes):
+#			var note_pos = (octave_pos + Vector2(0,j * space_between_notes)) + Vector2(0,vertical_offset)
+#			draw_line(note_pos,note_pos + Vector2(0,draw_width),Color.white)
 
 func update_zoom():
-	hzoom = clamp(hzoom,min_zoom,max_zoom)
+	hzoom = clamp(hzoom,min_h_zoom,max_h_zoom)
+	vzoom = clamp(vzoom,min_v_zoom,max_v_zoom)
 	space_between_bars = horizontal_scale * hzoom
+	space_between_notes = vertical_scale * vzoom
 	var bpm_factor = 100 as float / bpm
-	total_size = (space_between_bars * bars / timesig_denominator * bpm_factor) + 2
+	total_horizontal_size = (space_between_bars * bars / timesig_denominator * bpm_factor) + 2
+	total_vertical_size = space_between_notes * number_octaves * octave_notes 
 	update()
 
 func _draw():
 	clear_background()
+	draw_octaves()
 	draw_beats()
 	draw_bars()
--- views/MidiEditor/MidiEditor.gd
+++ views/MidiEditor/MidiEditor.gd
@@ -3,40 +3,58 @@ extends Control
 
 var previous_h_scroll : float
 var h_scrollbar : HScrollBar
+var v_scrollbar : VScrollBar
 
 onready var midi_draw_area : MidiDrawArea = $MidiDrawArea
 func _ready():
 	h_scrollbar = midi_draw_area.get_node("HScrollBar")
-	#get_v_scrollbar().connect("value_changed",self,"update_offsets")
+	v_scrollbar = midi_draw_area.get_node("VScrollBar")
+	v_scrollbar.connect("value_changed",self,"update_offsets")
 	h_scrollbar.connect("value_changed",self,"update_offsets")
 	connect("resized",self,"update_draw_area")
 	get_tree().get_root().connect("size_changed", self, "update_draw_area")
-	midi_draw_area.hzoom = min(midi_draw_area.min_zoom,midi_draw_area.max_zoom / 2)
-	midi_draw_area.update_zoom()
+	midi_draw_area.hzoom = min(midi_draw_area.min_h_zoom,midi_draw_area.max_h_zoom / 2)
+	midi_draw_area.vzoom = min(midi_draw_area.min_v_zoom,midi_draw_area.max_v_zoom / 2)
 	update_draw_area()
-	h_scrollbar.max_value = midi_draw_area.total_size
-	h_scrollbar.page = rect_size.x
-	print(h_scrollbar.max_value)
 
 func _input(event):
 	if event is InputEventMouseButton:
-		var previous_max_value = h_scrollbar.max_value
-		var previous_value = h_scrollbar.value
-		if event.button_index == BUTTON_WHEEL_UP and Input.is_key_pressed(KEY_CONTROL):
-			midi_draw_area.hzoom += midi_draw_area.zoom_steps
-			update_draw_area()
-		if event.button_index == BUTTON_WHEEL_DOWN and Input.is_key_pressed(KEY_CONTROL):
-			midi_draw_area.hzoom -= midi_draw_area.zoom_steps
-			update_draw_area()
-		h_scrollbar.max_value = midi_draw_area.total_size
-		h_scrollbar.value = previous_value * h_scrollbar.max_value / previous_max_value
+		if Input.is_key_pressed(KEY_CONTROL):
+			if Input.is_key_pressed(KEY_SHIFT):
+				var previous_max_value = h_scrollbar.max_value
+				var previous_value = h_scrollbar.value
+				if event.button_index == BUTTON_WHEEL_UP:
+					midi_draw_area.hzoom += midi_draw_area.zoom_steps
+					update_draw_area()
+				if event.button_index == BUTTON_WHEEL_DOWN and Input.is_key_pressed(KEY_CONTROL):
+					midi_draw_area.hzoom -= midi_draw_area.zoom_steps
+					update_draw_area()
+				h_scrollbar.max_value = midi_draw_area.total_horizontal_size
+				h_scrollbar.value = previous_value * h_scrollbar.max_value / previous_max_value
+			else:
+				var previous_max_value = v_scrollbar.max_value
+				var previous_value = v_scrollbar.value
+				if event.button_index == BUTTON_WHEEL_UP:
+					midi_draw_area.vzoom += midi_draw_area.zoom_steps
+					update_draw_area()
+				if event.button_index == BUTTON_WHEEL_DOWN and Input.is_key_pressed(KEY_CONTROL):
+					midi_draw_area.vzoom -= midi_draw_area.zoom_steps
+					update_draw_area()
+				v_scrollbar.max_value = midi_draw_area.total_vertical_size
+				v_scrollbar.value = previous_value * v_scrollbar.max_value / previous_max_value
+		
 
 func update_draw_area():
+	midi_draw_area.update_zoom()
+	h_scrollbar.max_value = midi_draw_area.total_horizontal_size
 	h_scrollbar.page = rect_size.x
+	v_scrollbar.max_value = midi_draw_area.total_vertical_size
+	v_scrollbar.page = rect_size.y
 	midi_draw_area.draw_width = rect_size.x
 	midi_draw_area.draw_height = rect_size.y
 	midi_draw_area.update_zoom()
 
 func update_offsets(value):
 	midi_draw_area.horizontal_offset = h_scrollbar.value
-	midi_draw_area.update()
+	midi_draw_area.vertical_offset = v_scrollbar.value
+	midi_draw_area.update_zoom()
