fb24297ccbda9522161d8c6857950f84852652f2
SeleDreams <seledreams.contact@gmail.com>
2021-08-09 16:57:52 +0200

    Changed to a manual scrollbar and fixed multiple rendering issues

--- views/MidiEditor/MidiDrawArea.gd
+++ views/MidiEditor/MidiDrawArea.gd
@@ -8,8 +8,8 @@ export var bpm : int = 120
 export var horizontal_scale : float = 50
 export var bars : int
 export var zoom_steps : float = 0.2
-export var max_zoom : float = 10
-
+export var max_zoom : float = 500
+export var min_zoom : float = 150
 
 onready var timesig : float = timesig_numerator as float / timesig_denominator
 onready var hzoom : float = 1
@@ -19,23 +19,41 @@ var vertical_offset : int
 var horizontal_offset : int
 var draw_width : int
 var draw_height : int
-
+var space_between_bars : float
+var actual_space_of_bar : float
+var total_size : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rect_min_size.x = horizontal_scale * bars
-	rect_min_size.y = 700
+	rect_min_size.y = draw_height
+	update_zoom()
 
 func clear_background():
-	draw_rect(Rect2(Vector2(horizontal_offset,vertical_offset),Vector2(draw_width,draw_height)),Color.black)
+	draw_rect(Rect2(Vector2(-horizontal_offset,0) + Vector2(horizontal_offset,vertical_offset),Vector2(draw_width,draw_height)),Color(40 as float / 255,40 as float / 255,40 as float / 255,1))
+
+func draw_beats():
+	var bpm_factor = 100 as float / bpm
+	actual_space_of_bar = space_between_bars + (timesig_denominator * bpm_factor)
+	for i in range(1,bars * timesig_numerator):
+			var beat_pos = Vector2(space_between_bars * i / timesig_numerator / timesig_denominator  * bpm_factor  ,0)
+			if beat_pos.x < rect_size.x + horizontal_offset and beat_pos.x > horizontal_offset:
+				draw_line(Vector2(-horizontal_offset,0) + beat_pos,Vector2(-horizontal_offset,0) + beat_pos + Vector2(0,draw_height),Color.gray)
 
 func draw_bars():
-	var space_between_bars : float = rect_size.x as float / bars
-	for i in range(0,bars):
-		if i * horizontal_scale >= horizontal_offset and i * horizontal_scale <= horizontal_offset + draw_width:
-			var line_pos : Vector2 = Vector2(space_between_bars * i,vertical_offset)
-			draw_line(line_pos,line_pos + Vector2(0,draw_height),Color.white)
+	var bpm_factor = 100 as float / bpm
+	for i in range(1,bars + 1):
+		var bar_pos = Vector2(space_between_bars * i / timesig_denominator * bpm_factor ,0)
+		if bar_pos.x < rect_size.x + horizontal_offset and bar_pos.x > horizontal_offset:
+			draw_line(Vector2(-horizontal_offset,0) + bar_pos,Vector2(-horizontal_offset,0) +  bar_pos + Vector2(0,draw_height),Color.white,2.0)
+
+func update_zoom():
+	hzoom = clamp(hzoom,min_zoom,max_zoom)
+	space_between_bars = horizontal_scale * hzoom
+	var bpm_factor = 100 as float / bpm
+	total_size = (space_between_bars * bars / timesig_denominator * bpm_factor) + 2
+	update()
 
 func _draw():
 	clear_background()
+	draw_beats()
 	draw_bars()
--- views/MidiEditor/MidiEditor.gd
+++ views/MidiEditor/MidiEditor.gd
@@ -1,19 +1,42 @@
-extends ScrollContainer
+tool
+extends Control
 
+var previous_h_scroll : float
+var h_scrollbar : HScrollBar
 
 onready var midi_draw_area : MidiDrawArea = $MidiDrawArea
-
 func _ready():
-	get_v_scrollbar().connect("value_changed",self,"update_offsets")
-	get_h_scrollbar().connect("value_changed",self,"update_offsets")
+	h_scrollbar = midi_draw_area.get_node("HScrollBar")
+	#get_v_scrollbar().connect("value_changed",self,"update_offsets")
+	h_scrollbar.connect("value_changed",self,"update_offsets")
 	connect("resized",self,"update_draw_area")
+	get_tree().get_root().connect("size_changed", self, "update_draw_area")
+	midi_draw_area.hzoom = min(midi_draw_area.min_zoom,midi_draw_area.max_zoom / 2)
+	midi_draw_area.update_zoom()
 	update_draw_area()
+	h_scrollbar.max_value = midi_draw_area.total_size
+	h_scrollbar.page = rect_size.x
+	print(h_scrollbar.max_value)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var previous_max_value = h_scrollbar.max_value
+		var previous_value = h_scrollbar.value
+		if event.button_index == BUTTON_WHEEL_UP and Input.is_key_pressed(KEY_CONTROL):
+			midi_draw_area.hzoom += midi_draw_area.zoom_steps
+			update_draw_area()
+		if event.button_index == BUTTON_WHEEL_DOWN and Input.is_key_pressed(KEY_CONTROL):
+			midi_draw_area.hzoom -= midi_draw_area.zoom_steps
+			update_draw_area()
+		h_scrollbar.max_value = midi_draw_area.total_size
+		h_scrollbar.value = previous_value * h_scrollbar.max_value / previous_max_value
 
 func update_draw_area():
+	h_scrollbar.page = rect_size.x
 	midi_draw_area.draw_width = rect_size.x
 	midi_draw_area.draw_height = rect_size.y
+	midi_draw_area.update_zoom()
 
 func update_offsets(value):
-	midi_draw_area.vertical_offset = scroll_vertical
-	midi_draw_area.horizontal_offset = scroll_horizontal
+	midi_draw_area.horizontal_offset = h_scrollbar.value
 	midi_draw_area.update()
